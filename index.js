7dcf5fdb13714f55ef7696b72b006080c33631cb
MikeVita4 <miguelvita13@gmail.com>
2020-09-24 14:46:17 +0000

    code

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 //require express
-var express = require('express')
+var express = require('express');
 
 //create express object, call express
-var app = express()
+var app = express();
 
-//get for homepage/
+//tell application to use EJS for templates
+app.set('view engine', 'ejs');
+
+//get home page /
 app.get('/', function(req, res){
     //return something to home page
     res.send('Hello World');
 });
-
 
 //server setup
 app.listen(3000, function(){
